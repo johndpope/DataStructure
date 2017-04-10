1ff00e339c2a32f55ad6356904c09d35ed491903
Yuji <hatsuneyuji@yahoo.co.jp>
2017-04-10 15:57:26 -0700

    use better terminology for bitmap, UInt64 support in 32bit system

---
+++ Tests/DataStructureTests/TestObjects.swift
@@ -0,0 +1,39 @@
+
+struct SharedTestValueType {
+    var _0 = 0
+    var _1 = 0
+    var _2 = 0
+    var _3 = 0
+    var _4 = 0
+    var _5 = 0
+    var _6 = 0
+    var _7 = 0
+    var _8 = 0
+
+    var i : Int {
+        return _0
+    } 
+
+    init() {}
+
+    init(_ val: Int) {
+        self._0 = val
+    }
+}
+
+class SharedTestRefType {
+    var _0 = SharedTestValueType()
+
+    var i: Int {
+        return _0.i
+    }
+
+    init() {}
+    init(_ v: Int) {
+        self._0 = SharedTestValueType(v)
+    }
+
+    deinit {
+
+    }
+}
